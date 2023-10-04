88222a6e5d1f78ae8a0e070ddbfdf0e14c41a879
Arjun vm <111493137+arjunvm07@users.noreply.github.com>
2023-10-04 10:49:35 +0530

    primenumber in range.py

---
+++ primenumber in range.py
@@ -0,0 +1,11 @@
+lower=int(input("enter the lower limit"))
+upper=int(input("enter the upper limit"))
+print("prime numbers between are")
+for num in range(lower,upper+1):
+    if num>1:
+        for i in range(2,num):
+            if(num%i==0):
+                break
+
+        else:
+            print(num)
