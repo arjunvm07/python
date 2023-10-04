648b938150d7bf2395c4d9337c0cc92cd1c8f393
Arjun vm <111493137+arjunvm07@users.noreply.github.com>
2023-10-04 09:25:45 +0530

    numberchecking.py

---
+++ numberchecking.py
@@ -0,0 +1,7 @@
+n=float(input("enter the number"))
+if n==0:
+    print("this number is zero",n)
+elif n>0:
+    print("this number is greater than zero",n)
+else:
+    print("this number is  less than zero",n)
