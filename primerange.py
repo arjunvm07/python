05d26326b4d2e71bd334f04a6229c8950836446a
Arjun vm <111493137+arjunvm07@users.noreply.github.com>
2023-12-25 21:23:01 +0530

    Delete primerange.py

--- primerange.py
+++
@@ -1,9 +0,0 @@
-l=int(input("Enter the lower bound"))
-u=int(input("Enter the upper bound"))
-for num in range(l,u+1):
-    if(num>1):
-        for i in range(2,num):
-            if(num%i==0):
-                break
-        else:
-            print(num)
